7b40ef1a3c90ea820a8d3871516278aab63f7f52
Nita2489 <neetapatil5194@gmail.com>
2022-02-11 18:31:48 +0530

    adding the projectfirstly

---
+++ src/test/java/com/visionit/automation/runners/TestRunner.java
@@ -0,0 +1,22 @@
+package com.visionit.automation.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="classpath:features",
+        glue="com.visionit.automation.stepdefs", 
+        tags="", 
+        plugin = {"pretty", 
+        		"html:target/html/htmlreport.html",
+                "json:target/json/file.json",
+            },
+        dryRun=false
+        )
+
+public class TestRunner {
+
+}
